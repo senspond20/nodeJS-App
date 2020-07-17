aa82ba735f524c986c9a144407f6795a1b12c2a3
senspond20 <senspond@gmail.com>
2020-07-18 07:27:59 +0900

    ejs test

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
@@ -6,5 +7,8 @@ express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
+  .engine('.html',require('ejs').renderFile)
+  .get('/', (req, res) => {
+       res.render('pages/index',{title: 'nodejs-app'})
+    })
   .listen(PORT, () => console.log(`Listening on http://localhost:${ PORT }`))
