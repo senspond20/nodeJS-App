73f335b77caf9aeb72269bc65ede3e660c8f15cd
senspond20 <senspond@gmail.com>
2020-07-19 02:25:45 +0900

    about

--- app.js
+++ app.js
@@ -3,12 +3,31 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
+const app = express();
+
+  // .use(express.static(path.join(__dirname, 'public')))
+
+// 템플릿 엔진 세팅
+app.set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .engine('.html',require('ejs').renderFile)
-  .get('/', (req, res) => {
-       res.render('pages/index',{title: 'nodejs-app'})
-    })
-  .listen(PORT, () => console.log(`Listening on http://localhost:${ PORT }`))
+ 
+// 리스닝시
+app.listen(PORT, () => console.log(`Listening on http://localhost:${ PORT }`))
+
+
+const siteData ={
+  title : "nodeJs",
+  //
+}
+
+// 라우터 랜더링
+app.get('/', (req, res) => {
+  res.render('pages/index',{title: "HOME |" + siteData.title})
+})
+
+app.get('/about',(req,res)=>{
+  res.render('pages/about',{title: "About |" + siteData.title})
+})
+
+
